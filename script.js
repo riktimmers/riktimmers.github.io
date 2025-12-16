d762c000ad7fe43593a11d08d44b45c13a7db94f
Rik <riktimmers@hotmail.com>
2025-12-16 23:01:34 +0100

    Test with multi file

---
+++ script.js
@@ -0,0 +1,18 @@
+function loadSection(file) {
+  fetch(file)
+    .then(response => {
+      if (!response.ok) throw new Error("File not found");
+      return response.text();
+    })
+    .then(html => {
+      document.getElementById('main-content').innerHTML = html;
+    })
+    .catch(error => {
+      document.getElementById('main-content').innerHTML = "<p>Error loading content</p>";
+      console.error(error);
+    });
+}
+
+// Load home by default
+loadSection('home.html');
+
